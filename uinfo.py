d429a033d1c212ab0bca49ecea5ae572f31dbf5b
0x5762 <dr3amnine@gmail.com>
2021-12-10 11:36:30 +0800

    修改代码结构
    修改代码结构，添加域名文件探测

--- uinfo.py
+++ uinfo.py
@@ -1,4 +1,5 @@
 #coding:utf-8
+import re
 import sys
 import csv
 import time
@@ -13,16 +14,17 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36'}
 
 def read_url_file(path):
-    print("\n[*] 加载文件：{} \n".format(path))
     ds = []
     with open(path) as f:
         for x in f:
             ds.append(x.strip())
-    print('共加载 {} 条记录\n'.format(len(ds)))
+    ds = list(set(ds))
     return ds
 
+'''
+    读取字典文件，去重排序
+'''
 def read_dicc_file(path):
-    print("\n[*] 加载文件：{} \n".format(path))
     ds = []
     ext_list = ['jsp','php','aspx']
     with open(path) as f:
@@ -38,26 +40,33 @@ def read_dicc_file(path):
                 ds.append(x.strip())
     ds = list(set(ds))
     ds.sort()
-    print('共加载 {} 条记录\n'.format(len(ds)))
     return ds
 
+'''
+    加载dirsearch结果的URL文件并去重
+'''
 def parse_dirsearch_file(path):
-    print("\n[*] 加载dirsearch文件：{} \n".format(path))
     ds = []
     with open(path) as f:
         for x in f:
             if x.strip() and '# Dirsearch' not in x:
-                res_list = x.split('   ')
-                ds.append(res_list[2].strip())
-    print('共加载 {} 条记录\n'.format(len(ds)))
+                res = re.findall('(\d+)\s+(\w*)\s+(.*)\s',x)
+                ds.append(res[0][2].strip())
+    ds = list(set(ds))
     return ds
 
-def write_to_csv(path,res_list):
-    with open(path,'w',newline='',encoding='utf-8') as csv_file:
-        csvwriter = csv.writer(csv_file)
-        csvwriter.writerow(['请求类型','请求URL','响应URL','响应状态码','响应长度','title','响应类型','server'])
-        for r in res_list:
-            csvwriter.writerow(r.split('\t'))
+def write_to_csv(path,res_list,mode='w',title=True):
+    if res_list:
+        print("[*] 写入文件：{} \n".format(path))
+        with open(path,mode,newline='',encoding='utf-8') as csv_file:
+            csvwriter = csv.writer(csv_file)
+            if title:
+                csvwriter.writerow(['请求类型','请求URL','响应URL','响应状态码','响应长度','title','响应类型','server'])
+            for r in res_list:
+                csvwriter.writerow(r.split('\t'))
+        print('[*] 共写入 {} 条记录\n'.format(len(res_list)))
+    else:
+        print('[*] 无记录不写入文件！\n')
 
 async def get_url_info(sem,url):
     try:
@@ -65,7 +74,7 @@ async def get_url_info(sem,url):
             async with httpx.AsyncClient(headers=headers,verify = False,follow_redirects = True) as client:
                 response = await client.get(url)
                 status_code = response.status_code
-                if status_code == 200 or status_code == 301 or status_code == 302 or status_code == 401:
+                if status_code == 200 or status_code == 401:
                     res_length = response.headers.get('content-length') if response.headers.get('content-length') else len(response.content)
                     if res_length > 1:
                         req_url = url
@@ -84,8 +93,8 @@ async def get_url_info(sem,url):
     except Exception as e:
         pass
 
-async def asyn_request(urls):
-        sem = asyncio.Semaphore(200)
+async def asyn_request(urls,sem):
+        sem = asyncio.Semaphore(sem)
         task_list = []  
         for url in urls:
             task = asyncio.create_task(get_url_info(sem, url))  
@@ -93,63 +102,88 @@ async def asyn_request(urls):
         return await asyncio.gather(*task_list) 
 
 def usage(msg=None):
-    print('Usage python '+sys.argv[0]+' [Options] use -h for help')
+    print('\n\tUsage python '+sys.argv[0]+' [Options] use -h for help\n')
     sys.exit()
     
-def begin(url_file,save_file,file_type='default',req_type='file',req_domain=''):
-    urls = []
-    if req_type == 'domain' and req_domain:
-        dir_url = read_dicc_file('dicc.txt')
-        req_domain = req_domain if req_domain.endswith('/') else req_domain+'/'
-        urls = list(map(lambda o:req_domain+o,dir_url))
-    elif file_type == 'dirasearch' :
-        urls = parse_dirsearch_file(url_file)
-    elif req_type == 'file':
-        urls = read_url_file(url_file)
-    print("[*] 开始探测.......................")
-    s = time.time()
+def begin_scan(urls,sem):
     loop = asyncio.get_event_loop()
-    res = loop.run_until_complete(asyn_request(urls))
-    e = time.time()
-    print("\n[*] over! 用时: {:.2f} 秒 \n".format(e-s))
-    res = list(filter(None, res))
-    if res:
-        print("[*] 写入文件：{} \n".format(save_file))
-        write_to_csv(save_file,res)
-        print('共写入 {} 条记录\n'.format(len(res)))
-    else:
-        print('[*] 无记录不写入文件！\n')
-    print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),'\n')
+    res = loop.run_until_complete(asyn_request(urls,sem))
+    return list(filter(None, res))
         
 
 if __name__ == '__main__':
     file_name = sys.argv[0]
-    parser = parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,epilog = 'Example: \n\tpython '+file_name+' -f url_file.txt \n\tpython '+file_name+' -fd dirsearch.txt \n\tpython '+file_name+' -u https://www.baidu.com\n\n\t\t\t\t\t\t\n',description='该程序主要用来探测URL信息,默认仅显示状态码为200、301、302、401的资源。')
+    parser = parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,epilog = 'Example: \n\tpython '+file_name+' -t file url_file.txt #加载URL文件\n\tpython '+file_name+' -t domain domains.txt #加载域名文件\n\tpython '+file_name+' -t dir dirsearch.txt #加载dirsearch结果的URL文件\n\tpython '+file_name+' -d https://www.baidu.com #扫描单个域名\n\n\t\t\t\t\t\t\n',description='该程序主要用来探测URL信息,返回对应的响应长度、title、server等字段，默认仅显示状态码为200、401的资源。')
     parser.error = usage
-    parser.add_argument('-f', '--file', help="需要请求的URL文件，为完整URL地址", required=False)
-    parser.add_argument('-fd', '--dirsearch_file', help="dirsearch结果的URL文件,自动解析", required=False)
-    parser.add_argument('-u', '--url', help="请求的主域名", required=False)
-    parser.add_argument('-d', '--dic', help="目录字典和-u参数一起使用，如果不加则默认为当前目录下dicc.txt", required=False)
-    parser.add_argument('-s', '--sem', help="并发的数量，默认200", required=False)
+    parser.add_argument('-t', '--type',help="请求的文件类型:\nfile 为内容全是完整url的文件\ndomain 为多个域名文件\ndir 为dirsearch的url文件\n默认为 file", default='file',choices=['file','domain','dir'],required=False)
+    parser.add_argument('-p', '--path', help="待请求的文件路径", required=False)
+    parser.add_argument('-d', '--domain', help="请求的单个主域名", required=False)
+    parser.add_argument('-w', '--word', help="目录字典和-d参数一起使用，如果不加则默认为当前目录下的dicc.txt", default='dicc.txt',required=False)
+    parser.add_argument('-s', '--sem', help="并发的数量，默认200", default=200, required=False)
     parser.add_argument('-ua', '--user-agent', help="请求的User-Agent,有默认UA", required=False)
-    parser.add_argument('-o', '--output', help="保存的文件名称,默认为result.csv", required=False)
+    parser.add_argument('-o', '--output', help="保存的文件名称,默认为result.csv", default='result.csv', required=False)
     
     args,unknown=parser.parse_known_args()
     
-    if unknown or len(sys.argv) < 2:
+    sem = args.sem
+    file_type = args.type
+    file_path = args.path
+    word_path = args.word
+    result_file = args.output
+    domain = args.domain
+    
+    if file_type and file_path == None and domain == None:
         usage()
     
-    url_file = args.file
-    req_domain = args.url
-    save_file = args.output if args.output else 'result.csv'
-    dirsearch_file = args.dirsearch_file
+    print(time.strftime("\n[*] %Y-%m-%d %H:%M:%S", time.localtime()))
+    print("[*] 程序启动 .......................\n")
+    start_time = time.time()
+    if domain:# 单个域名扫描
+        dicc_list = read_dicc_file(word_path)
+        print('[*] 加载字典文件 {} ,共 {} 条记录...'.format(word_path,len(dicc_list)))
+        new_domain = domain if domain.endswith('/') else domain+'/'
+        print('[*] 开始扫描域名 {} ...\n'.format(new_domain))
+        urls = list(map(lambda o:new_domain+o,dicc_list))
+        result = begin_scan(urls,sem)
+        end_time = time.time()
+        print("\n[*] 扫描域名 {} 结束,用时: {:.2f} 秒 \n".format(new_domain,end_time-start_time))
+        write_to_csv(result_file,result)
+    else:
+        if file_type == 'file':# URL文件扫描
+            print('[*] 加载URL文件 {} ,共 {} 条记录...'.format(file_path,len(read_dicc_file(file_path))))
+            urls = read_url_file(file_path)
+            result = begin_scan(urls,sem)
+            file_end_time = time.time()
+            print("\n[*] 扫描结束,用时: {:.2f} 秒 \n".format(file_end_time-start_time))
+            write_to_csv(result_file,result)
+        elif file_type == 'domain': # 域名文件扫描
+            domain_list = read_url_file(file_path)
+            dicc_list = read_dicc_file(word_path)
+            print('[*] 加载域名文件 {} ,共 {} 个域名...'.format(file_path,len(domain_list)))
+            print('[*] 加载字典文件 {} ,共 {} 条记录...\n'.format(word_path,len(dicc_list)))
+            i = 1
+            for domain in domain_list:
+                new_domain = domain if domain.endswith('/') else domain+'/'
+                print('[*] 探测第 {} 个域名 {} \n'.format(i,new_domain))
+                urls = list(map(lambda o:new_domain+o,dicc_list))
+                domain_start_time = time.time()
+                result = begin_scan(urls,sem)
+                domain_end_time = time.time()
+                print("\n\n[*] 探测 {} 结束,用时: {:.2f} 秒 \n".format(new_domain,domain_end_time-domain_start_time))
+                if i == 1:
+                    write_to_csv(result_file,result)
+                else:
+                    write_to_csv(result_file,result,mode='a+',title=False)
+                i+=1
+        elif file_type == 'dir':# dirsearch文件扫描
+            urls = parse_dirsearch_file(file_path)
+            print('[*] 加载dirsearch文件 {} ,共 {} 条记录...\n'.format(file_path,len(urls)))
+            result = begin_scan(urls,sem)
+            ds_end_time = time.time()
+            print("\n[*] 扫描结束,用时: {:.2f} 秒 \n".format(ds_end_time-start_time))
+            write_to_csv(result_file,result)
+    end_time = time.time()
+    print(time.strftime("\n[*] %Y-%m-%d %H:%M:%S", time.localtime()))
+    print("\n[*] 程序扫描结束，共计用时: {:.2f} 秒 \n".format(end_time-start_time))
     
-    if url_file and dirsearch_file == None:
-        begin(url_file,save_file)
-    
-    if dirsearch_file:
-        begin(dirsearch_file,save_file,file_type = 'dirasearch')
-    
-    if req_domain and dirsearch_file == None and url_file == None:
-        begin(dirsearch_file,save_file,file_type = 'dirasearch',req_type='domain',req_domain=req_domain)
     
