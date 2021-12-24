ed19530444b14ebd18437c607f1f6734281b2c16
0x5762 <dr3amnine@gmail.com>
2021-12-24 11:43:58 +0800

    增加对通用404的判断

--- uinfo.py
+++ uinfo.py
@@ -4,6 +4,9 @@ import sys
 import csv
 import time
 import httpx
+import random
+import string
+import hashlib
 import asyncio
 import argparse
 from pathlib import Path
@@ -12,6 +15,27 @@ from urllib.parse import urlparse
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36'}
+
+def genearate_md5_sign(data):
+    if data:
+        return hashlib.md5(data.encode(encoding='utf-8')).hexdigest()
+    
+'''
+    随机生成url，请求获取结果生成md5签名
+'''
+def probe_sign(domain):
+    random_strs = ''.join(random.choice(string.ascii_letters+string.digits) for i in range(10))
+    url = domain+random_strs+'.'+random_strs
+    print('[*] 生成随机URL {}\n'.format(url))
+    sign = None
+    try:
+        res = httpx.get(url,headers=headers,verify = False,follow_redirects = True)
+        sign = genearate_md5_sign(res.text)
+        print('[*] 响应签名为： {}\n'.format(sign))
+        return sign
+    except:
+        pass
+    return sign
 
 def read_url_file(path):
     ds = []
@@ -68,36 +92,50 @@ def write_to_csv(path,res_list,mode='w',title=True):
     else:
         print('[*] 无记录不写入文件！\n')
 
-async def get_url_info(sem,url):
+async def get_url_info(sem,url,ex_content_sign=None):
     try:
         async with sem:
             async with httpx.AsyncClient(headers=headers,verify = False,follow_redirects = True) as client:
                 response = await client.get(url)
                 status_code = response.status_code
-                if status_code == 200 or status_code == 401:
+                if status_code == 200:
                     res_length = response.headers.get('content-length') if response.headers.get('content-length') else len(response.content)
                     if float(res_length) > 1:
                         req_url = url
                         res_url = response.url
                         content = response.text
-                        req_suffix = Path(urlparse(url).path).suffix[1:]
-                        req_suffix = req_suffix if req_suffix else 'None'
-                        select_obj = Selector(text=content).xpath('//title/text()').get()
-                        title = select_obj if select_obj else 'None'
-                        res_content_type = response.headers.get('content-type') if response.headers.get('content-type') else 'None'
-                        res_content_type = res_content_type[:res_content_type.find(';')] if res_content_type.find(';')>-1 else res_content_type
-                        res_server = response.headers.get('server') if response.headers.get('server') else 'None'
-                        strs = '{}\t{}\t{}\t{}\t{:.2f}KB\t{}\t{}\t{}'.format(req_suffix,req_url,res_url,status_code,float(res_length)/1024,title,res_content_type,res_server)
-                        print(strs)
-                        return strs
+                        if ex_content_sign:
+                            content_sign = genearate_md5_sign(content)
+                            if content_sign != ex_content_sign:
+                                req_suffix = Path(urlparse(url).path).suffix[1:]
+                                req_suffix = req_suffix if req_suffix else 'None'
+                                select_obj = Selector(text=content).xpath('//title/text()').get()
+                                title = select_obj if select_obj else 'None'
+                                res_content_type = response.headers.get('content-type') if response.headers.get('content-type') else 'None'
+                                res_content_type = res_content_type[:res_content_type.find(';')] if res_content_type.find(';')>-1 else res_content_type
+                                res_server = response.headers.get('server') if response.headers.get('server') else 'None'
+                                strs = '{}\t{}\t{}\t{}\t{:.2f}KB\t{}\t{}\t{}'.format(req_suffix,req_url,res_url,status_code,float(res_length)/1024,title,res_content_type,res_server)
+                                print(strs)
+                                return strs
+                        else:
+                            req_suffix = Path(urlparse(url).path).suffix[1:]
+                            req_suffix = req_suffix if req_suffix else 'None'
+                            select_obj = Selector(text=content).xpath('//title/text()').get()
+                            title = select_obj if select_obj else 'None'
+                            res_content_type = response.headers.get('content-type') if response.headers.get('content-type') else 'None'
+                            res_content_type = res_content_type[:res_content_type.find(';')] if res_content_type.find(';')>-1 else res_content_type
+                            res_server = response.headers.get('server') if response.headers.get('server') else 'None'
+                            strs = '{}\t{}\t{}\t{}\t{:.2f}KB\t{}\t{}\t{}'.format(req_suffix,req_url,res_url,status_code,float(res_length)/1024,title,res_content_type,res_server)
+                            print(strs)
+                            return strs
     except Exception as e:
         pass
 
-async def asyn_request(urls,sem):
+async def asyn_request(urls,sem,ex_content_sign=None):
         sem = asyncio.Semaphore(sem)
         task_list = []  
         for url in urls:
-            task = asyncio.create_task(get_url_info(sem, url))  
+            task = asyncio.create_task(get_url_info(sem, url,ex_content_sign))  
             task_list.append(task)
         return await asyncio.gather(*task_list) 
 
@@ -105,15 +143,15 @@ def usage(msg=None):
     print('\n\tUsage python '+sys.argv[0]+' [Options] use -h for help\n')
     sys.exit()
     
-def begin_scan(urls,sem):
+def begin_scan(urls,sem,ex_content_sign=None):
     loop = asyncio.get_event_loop()
-    res = loop.run_until_complete(asyn_request(urls,sem))
+    res = loop.run_until_complete(asyn_request(urls,sem,ex_content_sign))
     return list(filter(None, res))
         
 
 if __name__ == '__main__':
     file_name = sys.argv[0]
-    parser = parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,epilog = 'Example: \n\tpython '+file_name+' -t file url_file.txt #加载URL文件\n\tpython '+file_name+' -t domain domains.txt #加载域名文件\n\tpython '+file_name+' -t dir dirsearch.txt #加载dirsearch结果的URL文件\n\tpython '+file_name+' -d https://www.baidu.com #扫描单个域名\n\n\t\t\t\t\t\t\n',description='该程序主要用来探测URL信息,返回对应的响应长度、title、server等字段，默认仅显示状态码为200、401的资源。')
+    parser = parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,epilog = 'Example: \n\tpython '+file_name+' -t file url_file.txt #加载URL文件\n\tpython '+file_name+' -t domain domains.txt #加载域名文件\n\tpython '+file_name+' -t dir dirsearch.txt #加载dirsearch结果的URL文件\n\tpython '+file_name+' -d https://www.baidu.com #扫描单个域名\n\n\t\t\t\t\t\t\n',description='该程序主要用来探测URL信息,返回对应的响应长度、title、server等字段，默认仅显示状态码为200的资源。')
     parser.error = usage
     parser.add_argument('-t', '--type',help="请求的文件类型:\nfile 为内容全是完整url的文件\ndomain 为多个域名文件\ndir 为dirsearch的url文件\n默认为 file", default='file',choices=['file','domain','dir'],required=False)
     parser.add_argument('-p', '--path', help="待请求的文件路径", required=False)
@@ -144,7 +182,8 @@ if __name__ == '__main__':
         new_domain = domain if domain.endswith('/') else domain+'/'
         print('[*] 开始扫描域名 {} ...\n'.format(new_domain))
         urls = list(map(lambda o:new_domain+o,dicc_list))
-        result = begin_scan(urls,sem)
+        sign = probe_sign(new_domain)
+        result = begin_scan(urls,sem,sign)
         end_time = time.time()
         print("\n[*] 扫描域名 {} 结束,用时: {:.2f} 秒 \n".format(new_domain,end_time-start_time))
         write_to_csv(result_file,result)
@@ -167,7 +206,8 @@ if __name__ == '__main__':
                 print('[*] 探测第 {} 个域名 {} \n'.format(i,new_domain))
                 urls = list(map(lambda o:new_domain+o,dicc_list))
                 domain_start_time = time.time()
-                result = begin_scan(urls,sem)
+                sign = probe_sign(new_domain)
+                result = begin_scan(urls,sem,sign)
                 domain_end_time = time.time()
                 print("\n\n[*] 探测 {} 结束,用时: {:.2f} 秒 \n".format(new_domain,domain_end_time-domain_start_time))
                 if i == 1:
@@ -185,5 +225,4 @@ if __name__ == '__main__':
     end_time = time.time()
     print(time.strftime("\n[*] %Y-%m-%d %H:%M:%S", time.localtime()))
     print("\n[*] 程序扫描结束，共计用时: {:.2f} 秒 \n".format(end_time-start_time))
-    
     
